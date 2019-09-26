47d381a3f026032068e70742b003253a25ab7b32
Giuseppe <giuseppe.sapienza@xcoding.it>
2019-09-26 16:58:53 +0200

    tutorial completed

--- SwiftUI-State/ContentView.swift
+++ SwiftUI-State/ContentView.swift
@@ -9,8 +9,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pizze: Int = 0
+    @State var cokes: Int = 0
+    @State var address: String = ""
+    @State var premiumDelivery: Bool = false
+    
+    let pizzaPrice: Double = 11.50
+    let cokePrice: Double = 2.50
+    let premiumDeliveryPrice: Double = 8.0
+    
+    var finalPrice: Double {
+        (Double(pizze) * pizzaPrice)
+        +
+        (Double(cokes) * cokePrice)
+        +
+        (premiumDelivery ? Double(premiumDeliveryPrice) : 0)
+    }
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack {
+                Text("Menu")
+                    .font(.largeTitle)
+                    .padding(.bottom, 20)
+                
+                Spacer()
+                
+                Button("Resetta", action: {
+                    self.pizze = 0
+                    self.cokes = 0
+                })
+            }
+            
+            Stepper(value: $pizze, in: 0...10, label: {
+                Text("Pizza Margherita")
+                Text("\(pizze)")
+            })
+            
+            Stepper(value: $cokes, in: 0...10, label: {
+                Text("Coca Cola")
+                Text("\(cokes)")
+            })
+            
+            HStack {
+                Text("Prezzo finale")
+                Spacer()
+                Text("\(format(x: finalPrice))")
+            }
+            
+            if (finalPrice > 0) {
+                Text("Indirizzo di spedizione")
+                TextField("Via...", text: $address)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Toggle(isOn: $premiumDelivery, label: {
+                    Text("Spedizione Prioritaria")
+                })
+                
+            }
+            
+        }
+    }
+    
+    func format(x: Double) -> String {
+        String(format: "%.2f", x)
     }
 }
 
